c4a7c501a84741f8978c0f6fc3076e5a3902ce50
Eduardo Beretta <eduardo.berettarod@gmail.com>
2026-08-20 22:14:48 -0300

    refactor: usar FlatList em Profile
    Substitui ScrollView por FlatList no screen Profile para aproveitar ListHeaderComponent/ListFooterComponent, ItemSeparatorComponent e melhor desempenho. Removi o import não utilizado de ScrollView e ajustei contentContainerStyle e layout dos itens

--- src/app/screen/Profile/index.tsx
+++ src/app/screen/Profile/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, FlatList, ScrollView } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import React from 'react'
 import HeaderSection from '@/components/HeaderSection';
 import { styles } from './style';
@@ -44,39 +44,56 @@ export default function Profile() {
   const insets = useSafeAreaInsets();
 
   const renderItem = ({ item }: { item: DataItem }) => (
-    <CardProfile icon={item.icon} text={item.text} info={item.info} />
+    <CardProfile
+      icon={item.icon}
+      text={item.text}
+      info={item.info}
+    />
   );
 
   return (
-    <ScrollView contentContainerStyle={[
-      styles.container, {
-        paddingTop: insets.top, paddingBottom: insets.bottom + 120
-      }
-    ]}>
-      <View style={styles.header}>
-        <IconProfile />
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.text}
+      renderItem={renderItem}
 
-        <View style={styles.info}>
-          <Text style={styles.user}>Eduardo Beretta</Text>
-          <Text style={styles.diabetes}>Diabetes • Tipo 1</Text>
+      ItemSeparatorComponent={() => (
+        <Separator color={colors.gray[700]} />
+      )}
+
+      contentContainerStyle={[
+        styles.container,
+        {
+          paddingTop: insets.top,
+          paddingBottom: insets.bottom + 120,
+        },
+      ]}
+
+      ListHeaderComponent={
+        <View style={styles.header}>
+          <IconProfile />
+
+          <View style={styles.info}>
+            <Text style={styles.user}>
+              Eduardo Beretta
+            </Text>
+
+            <Text style={styles.diabetes}>
+              Diabetes • Tipo 1
+            </Text>
+          </View>
         </View>
-      </View>
+      }
 
-      <FlatList
-        data={data}
-        keyExtractor={(item) => item.text}
-        renderItem={renderItem}
-        ItemSeparatorComponent={() => <Separator color={colors.gray[700]} />}
-        contentContainerStyle={{ paddingBottom: 16 }}
-      />
-
-      <View style={{ marginTop: 24 }}>
-        <Button
-          title={'SAIR'}
-          borderColor={colors.gray[700]}
-          colorText={colors.red[400]}
-        />
-      </View>
-    </ScrollView>
+      ListFooterComponent={
+        <View style={{ marginTop: 24 }}>
+          <Button
+            title="SAIR"
+            borderColor={colors.gray[700]}
+            colorText={colors.red[400]}
+          />
+        </View>
+      }
+    />
   );
 }
